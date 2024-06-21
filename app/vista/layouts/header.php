32230e3c54a62fbf3834bbbd63bf36a98c7a5aae
JacobLuqueEntenza <jacobluquentenza@gmail.com>
2024-06-21 21:39:33 +0200

    css header terminado

--- app/vista/layouts/header.php
+++ app/vista/layouts/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista</title>
-    <link rel="stylesheet" href="/tutrastero/tutrastero/public/css/estilos.css">
+    <link rel="stylesheet" href="../public/css/estilos.css">
     <!-- Enlaza los estilos de Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">      
@@ -13,40 +13,24 @@
 <body>
 
     <header class="header" id="header">
-        <p class="p-header"><a href="/tutrastero/tutrastero/public/index.php">Inicio</a> </p>
-        <p class="p-header"><a href="/tutrastero/tutrastero/app/vista/trasteros/trasterosLista.php">Trasteros</a> </p>
-        <p class="p-header"><a href="/tutrastero/tutrastero/app/vista/layouts/contacto.php">Contacto</a> </p>
-    <?php  
-        session_start();
-        //si el rol es distinto de tres nos aparecera pagos y recibos
-        if (isset($_SESSION['usuario']) && isset($_SESSION['rol']) && $_SESSION['rol'] != 3){ 
-            if($_SESSION['rol'] ==1){?>
-                <p class="p-header"><a href="/tutrastero/tutrastero/app/vista/averias/averiasLista.php">Averias</a> </p>
-            <?php 
-            }else{?>
-                <p class="p-header"><a href="/tutrastero/tutrastero/app/vista/averias/averiaNuevo.php">Averias</a> </p>
-            <?php } ?>        
-        <p class="p-header"><a href="/tutrastero/tutrastero/app/vista/recibos/recibosLista.php">Recibos</a> </p>
-        <?php } 
-        if (isset($_SESSION['usuario']) && isset($_SESSION['rol']) && $_SESSION['rol'] == 1){ ?>
-        <p class="p-header"><a href="/tutrastero/tutrastero/app/vista/usuarios/usuariosLista.php" onclick="borrarCliente();">Usuarios</a> </p>
-
-    <?php } ?>    
-    <?php 
-        
-        if(!isset($_SESSION['usuario'])){?>
-
-            <p class="p-header"><img src="/tutrastero/tutrastero/public/img/Login.ico" alt="login"><a href="/tutrastero/tutrastero/app/vista/login/login.php">Login</a></p>  
-            <p class="p-header"><a href="/tutrastero/tutrastero/app/vista/usuarios/usuarioNuevo.php">Registrate</a></p> 
-
-    <?php
-            }else{ ?>
-                <p class="p-header"><a href="">Hola, <?php echo $_SESSION['usuario']; ?>  </a></p>
-                <p class="p-header"><a href="/tutrastero/tutrastero/app/controlador/cerrarSesionControlador.php">Cerrar Sesión</a></p>
-        
-    <?php
-    };
-    ?>       
-
+        <nav class="navbar">
+            <h3 class="nombre">tutrasteroenhuelva.es</h3>
+            <input  class="menu" type="checkbox"  id="menu">
+            <label for="menu" class="lblHamburguesa">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="hamburguesa" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                </svg>
+            </label>
+            <ul class="ulMenu">
+                <li class="liMenu"><a class="aMenu" href="#">Inicio</a> </li>
+                <li class="liMenu"><a class="aMenu" href="#">Trasteros</a> </li>
+                <li class="liMenu"><a class="aMenu" href="#">Contacto</a> </li>
+                <li class="liMenu"><a class="aMenu" href="#">Averias</a> </li>
+                <li class="liMenu"><a class="aMenu" href="#">Recibos</a> </li>
+                <li class="liMenu"><a class="aMenu" href="#">Login</a> </li>
+                <li class="liMenu"><a class="aMenu" href="#">Registrate</a> </li>
+                <li class="liMenu"><a class="aMenu" href="#">Cerrar Sesión</a> </li>
+            </ul> 
+        </nav>
     </header>
         
--- public/index.php
+++ public/index.php
@@ -1,4 +1,118 @@
 <?php 
     // Incluir el header 
     include '../app/vista/layouts/header.php'; 
+
+   
 ?>
+
+<section class="principal">        
+        <h2 id="titulo">tutrasteroenhuelva.es</h2>
+        <h3 id="parrafo">Simplifica tu almacenamiento y mantén tus pertenencias seguras con nuestros trasteros en alquiler.</h3>
+        <div class="botones">            
+            <button onclick="window.location.href='/tutrastero/tutrastero/app/vista/trasteros/trasterosLista.php#situacionTrasteros' ">Explorar Trasteros</button>
+        </div>
+</section>
+<section class="grid-contenedor1">
+        <h1>Catálogo de Trasteros</h1>
+        <div class="tarjeta">
+            <h2>Tamaños Disponibles</h2>
+            <p>Desde pequeños hasta extra grandes, tenemos una variedad de trasteros para cubrir todas tus necesidades de almacenamiento.</p>
+        </div>
+        <div class="tarjeta">
+            <h2>Ubicaciones Estratégicas</h2>
+            <p>Nuestros trasteros están ubicados en lugares convenientes y de fácil acceso en toda la ciudad.</p>
+        </div>
+        <div class="tarjeta">
+            <h2>Seguridad Garantizada</h2>
+            <p>Tus pertenencias estarán protegidas con nuestros sistemas de vigilancia y acceso controlado.</p>
+        </div>
+</section>
+
+<section class="grid-contenedor2">        
+        <h1>Proceso de Alquiler</h1> 
+
+        <div class="tarjeta" id="tarj-1">
+            <h1>1</h1>
+        </div>
+        <div class="tarjeta" id="tarj-2">
+            <img src="img/flechaDcha.png" alt="flechaDerecha">
+        </div> 
+        <div class="tarjeta" id="tarj-3">            
+            <h2>Selección</h2>            
+            <p>Elige el trastero que mejor se ajuste a tus necesidades y presupuesto.</p>
+        </div>
+        <div class="tarjeta" id="tarj-4">
+                
+        </div>
+        <div class="tarjeta" id="tarj-5">            
+            <h2>Registro</h2>            
+            <p>Completa el formulario de registro y firma el contrato de alquiler.</p>
+        </div>
+        <div class="tarjeta" id="tarj-6">
+            <img src="img/flechaIzq.png" alt="flechaIzquierda">
+        </div>
+        <div class="tarjeta" id="tarj-7">
+            <h1>2</h1>
+        </div>  
+        <div class="tarjeta" id="tarj-8">            
+        </div>      
+        <div class="tarjeta" id="tarj-9">
+            <h1>3</h1>
+        </div>
+        <div class="tarjeta" id="tarj-10">
+            <img src="img/flechaDcha.png" alt="flechaDerecha">
+        </div>
+        <div class="tarjeta" id="tarj-11">            
+            <h2>Seguridad Garantizada</h2>            
+            <p>Recibe tu llave y comienza a utilizar tu trastero de manera segura.</p>
+        </div>
+</section>
+<section class="grid-contenedor3">
+        <h1>Cobro de Recibos y Facturación</h1>
+        <div class="tarjeta">
+            <h2>Pagos Automáticos</h2>
+            <p>Configura el pago automático de tus recibos para evitar retrasos.</p>
+        </div>
+        <div class="tarjeta">
+            <h2>Recordatorios de Vencimiento</h2>
+            <p>Recibe notificaciones oportunas sobre tus próximos pagos.</p>
+        </div>
+        <div class="tarjeta">
+            <h2>Historial de Pagos</h2>
+            <p>Accede fácilmente a tus registros de pagos en línea.</p>
+        </div>
+        <div class="tarjeta">
+            <h2>Opciones de Pago</h2>
+            <p>Paga en efectivo, tarjeta de crédito, bizum o transferencia bancaria.</p>
+        </div>
+</section>
+
+<section class="grid-contenedor4">
+
+        <h1>Contacto y Soporte</h1>
+        <div class="tarjeta">
+            <img src="/tutrastero/tutrastero/public/img/gmail.png" alt="gmail">
+            <img src="/tutrastero/tutrastero/public/img/outlook.png" alt="outlook">
+            <h2>Correo Electrónico</h2>
+            <p>Envíanos tus consultas y solicitudes a tutrasteroenhuelva@gmail.com</p>
+        </div>
+        <div class="tarjeta">
+            <img src="/tutrastero/tutrastero/public/img/telefono.png" alt="telefono">
+            <img src="/tutrastero/tutrastero/public/img/whatapp.png" alt="whatapp">
+            <h2>Línea de Atención</h2>
+            <p>Llámanos al 555-1234 para obtener asistencia personalizada.</p>
+        </div>
+        
+</section>
+
+<section class="mapa">
+
+        <h1>Donde Estamos:</h1>
+        <div class="tarjeta">
+            <p>Nuestras instalaciones se encuantra ubicada en local comercial con salida a dos calles, de fácil acceso.</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2618.5861172929244!2d-6.943239264047862!3d37.281381421458924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd11cfc22776e743%3A0x827feb3310ef61c!2sC.%20Legi%C3%B3n%20Espa%C3%B1ola%2C%206%2C%2021005%20Huelva!5e0!3m2!1ses!2ses!4v1714458476831!5m2!1ses!2ses"style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        
+        
+</section>
+
