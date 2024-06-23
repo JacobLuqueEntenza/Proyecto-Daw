2b7f13a644fd458d456debf99a2149c7f7ea59d4
JacobLuqueEntenza <jacobluquentenza@gmail.com>
2024-06-23 04:06:45 +0200

    index terminado css

--- public/index.php
+++ public/index.php
@@ -33,19 +33,19 @@
         <div class="divAlquiler">
             <div class="tarjeta" id="tarj-1">1</div>            
             <div class="tarjeta">            
-                <h2>Selección</h2>            
+                <h3>Selección</h3>            
                 <p>Elige el trastero que mejor se ajuste a tus necesidades y presupuesto.</p>
             </div>
             <img src="img/flechaAbj.png" alt="flechaDerecha">
             <div class="tarjeta" id="tarj-2">2</div>            
             <div class="tarjeta" >            
-                <h2>Registro</h2>            
+                <h3>Registro</h3>            
                 <p>Completa el formulario de registro y firma el contrato de alquiler.</p>
             </div>
             <img src="img/flechaAbj.png" alt="flechaDerecha">
             <div class="tarjeta" id="tarj-3">3</div>            
             <div class="tarjeta" >            
-                <h2>Seguridad Garantizada</h2>            
+                <h3>Seguridad Garantizada</h3>            
                 <p>Recibe tu llave y comienza a utilizar tu trastero de manera segura.</p>
             </div>
         </div>    
@@ -54,19 +54,19 @@
         <h2>Cobro de Recibos y Facturación</h2>
         <div class="divCobros">
             <div class="tarjeta">
-                <h2>Pagos Automáticos</h2>
+                <h3>Pagos Automáticos</h3>
                 <p>Configura el pago automático de tus recibos para evitar retrasos.</p>
             </div>
             <div class="tarjeta">
-                <h2>Recordatorios de Vencimiento</h2>
+                <h3>Recordatorios de Vencimiento</h3>
                 <p>Recibe notificaciones oportunas sobre tus próximos pagos.</p>
             </div>
             <div class="tarjeta">
-                <h2>Historial de Pagos</h2>
+                <h3>Historial de Pagos</h3>
                 <p>Accede fácilmente a tus registros de pagos en línea.</p>
             </div>
             <div class="tarjeta">
-                <h2>Opciones de Pago</h2>
+                <h3>Opciones de Pago</h3>
                 <p>Paga en efectivo, tarjeta de crédito, bizum o transferencia bancaria.</p>
             </div>
         </div>
@@ -80,7 +80,7 @@
                     <img src="img/gmail.png" alt="gmail">
                     <img src="img/outlook.png" alt="outlook">
                 </div>    
-                <h2>Correo Electrónico</h2>
+                <h3>Correo Electrónico</h3>
                 <p>Envíanos tus consultas y solicitudes a tutrasteroenhuelva@gmail.com</p>
             </div>
             <div class="tarjeta">
@@ -88,14 +88,14 @@
                     <img src="img/telefono.png" alt="telefono">
                     <img src="img/whatapp.png" alt="whatapp">
                 </div>
-                <h2>Línea de Atención</h2>
+                <h3>Línea de Atención</h3>
                 <p>Llámanos al 555-1234 para obtener asistencia personalizada.</p>
             </div>
         </div>
 </section>
 
 <section class="mapa">
-        <h1>Donde Estamos:</h1>
+        <h2>Donde Estamos:</h2>
         <div class="tarjeta">
             <p>Nuestras instalaciones se encuantra ubicada en local comercial con salida a dos calles, de fácil acceso.</p>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2618.5861172929244!2d-6.943239264047862!3d37.281381421458924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd11cfc22776e743%3A0x827feb3310ef61c!2sC.%20Legi%C3%B3n%20Espa%C3%B1ola%2C%206%2C%2021005%20Huelva!5e0!3m2!1ses!2ses!4v1714458476831!5m2!1ses!2ses"style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
